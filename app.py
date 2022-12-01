71383d7bff743390d7fd877cbce9e4d7b060a16c
Gaurav Sarkar <60139335+Gaurav7888@users.noreply.github.com>
2022-12-01 16:23:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+st.markdown("# Main page")
+st.sidebar.markdown(" :)( ")
+
+st.title("The AIML Tutor")
+
+
+
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="You:{}\n".format(txt),
+  temperature=0.3,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.5,
+  presence_penalty=0.0,
+  stop=["You:"]
+)
+
+st.write(response["choices"])
+
